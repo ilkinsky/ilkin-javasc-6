3777247372f3b7f68837756710afd3bafd039474
ilkinsky <ilkinehmedzade35@gmail.com>
2022-06-18 11:30:29 +0400

    ilkin-javasc-6

---
+++ ilkin.js
@@ -0,0 +1,31 @@
+let nomre1 = {
+    ad: "İlkin",
+    soyad:"Əhmədzadə",
+    yas:11,
+}
+
+let nomre2 = {
+    ad: "Nərmin",
+    soyad:"Həsənova",
+    yas:28,
+}
+
+let nomre3 = {
+    ad: "Kamal",
+    soyad:"Şəmşiyev",
+    yas:13,
+}
+
+let ad = prompt("adivizi daxil edin")
+let soyad = prompt("soy adivizi daxil edin")
+let yas = prompt("yasivizi daxil edin")
+
+
+let person = {
+    firstname: ad,
+    lastname: soyad,
+    age: yas
+
+}
+
+console.log(person)
